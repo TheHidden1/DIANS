4358d4f085b2dfaf0092200f4f0b23bf371d1b0b
TheHidden1 <149437925+TheHidden1@users.noreply.github.com>
2023-11-29 22:00:15 +0100

    Delete Домашно бр.2 directory

--- Домашно бр.2/tech_prototype/macedonian_heritage/src/main/java/finki/ukim/mk/macedonian_heritage/model/exceptions/InvalidArgumentsException.java
+++
@@ -1,8 +0,0 @@
-package finki.ukim.mk.macedonian_heritage.model.exceptions;
-
-public class InvalidArgumentsException extends RuntimeException{
-    public InvalidArgumentsException() {
-        super("Invalid argument.");
-    }
-
-}
--- Домашно бр.2/tech_prototype/macedonian_heritage/src/main/java/finki/ukim/mk/macedonian_heritage/services/UsersService.java
+++
@@ -1,9 +0,0 @@
-package finki.ukim.mk.macedonian_heritage.services;
-
-import finki.ukim.mk.macedonian_heritage.model.Users;
-
-import java.util.List;
-
-public interface UsersService {
-    List<Users> findAllUsers();
-}
