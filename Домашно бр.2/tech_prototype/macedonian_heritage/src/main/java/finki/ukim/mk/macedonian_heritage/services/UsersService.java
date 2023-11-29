a31e3f94d233083fb1403b0a6afa06d3a98236a2
TheHidden1 <149437925+TheHidden1@users.noreply.github.com>
2023-11-29 21:54:26 +0100

    homework1_start

---
+++ Домашно бр.2/tech_prototype/macedonian_heritage/src/main/java/finki/ukim/mk/macedonian_heritage/services/UsersService.java
@@ -0,0 +1,9 @@
+package finki.ukim.mk.macedonian_heritage.services;
+
+import finki.ukim.mk.macedonian_heritage.model.Users;
+
+import java.util.List;
+
+public interface UsersService {
+    List<Users> findAllUsers();
+}
