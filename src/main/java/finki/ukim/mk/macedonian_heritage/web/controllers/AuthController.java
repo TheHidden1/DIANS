f40c7ae5160c9d7a0447e36f7b7460a0d06dfcf8
TheHidden1 <149437925+TheHidden1@users.noreply.github.com>
2023-12-13 20:23:05 +0100

    cors policy changing

--- src/main/java/finki/ukim/mk/macedonian_heritage/web/controllers/AuthController.java
+++ src/main/java/finki/ukim/mk/macedonian_heritage/web/controllers/AuthController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/auth")
 //@CrossOrigin(origins = "http://localhost:5174/", allowCredentials = "true")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "http://localhost:5173/")
 public class AuthController {
     private final AuthService authService;
 
--- src/main/java/finki/ukim/mk/macedonian_heritage/web/controllers/ObjectsController.java
+++ src/main/java/finki/ukim/mk/macedonian_heritage/web/controllers/ObjectsController.java
@@ -13,7 +13,6 @@ import java.util.Optional;
 
 @RestController
 @Validated
-@CrossOrigin(origins = "*")
 @RequestMapping(name="All Objects", value = "/api/v1/")
 public class ObjectsController {
     private final ObjectsServices objectsServices;
